1261f528342cf2baa9c5ae55869b24e1f4c97c58
shaqulle lee <17631288710@163.com>
2021-11-13 17:17:05 +0800

    pytest mark的使用

--- testPytest/test_1.py
+++ testPytest/test_1.py
@@ -4,27 +4,33 @@ Author: lishaogang
 version: 
 Date: 2021-11-13 16:16:38
 LastEditors: lishaogang
-LastEditTime: 2021-11-13 16:42:27
+LastEditTime: 2021-11-13 17:12:16
 '''
 
 import pytest
 
+
 class TestA():
     def test_a1(self):
-        assert 1==1
-    
+        assert 1 == 1
+
     def test_a2(self):
-        assert 2==2
+        assert 2 == 2
 
-def test_1():
-    assert 12==12
 
-def test_2():
+@pytest.mark.a
+def test1():
+    assert 12 == 12
+
+
+@pytest.mark.b
+def test2():
     print(555)
     assert(12 == int('12'))
 
 
 if __name__ == "__main__":
     # pytest.main(['-s', __file__])  # -s:将函数中的print的结果打印出来
-    pytest.main(['-v', r'test_1.py']) # -v:將測試結果詳細列出
-    # pytest.main(['-q', r'F:/githubfiles/PythonAutoTest/testPytest']) #-q:简略输出测试结果
+    # pytest.main(['-v', r'test_1.py']) # -v:將測試結果詳細列出
+    # -q:简略输出测试结果
+    pytest.main(['-q', r'F:/githubfiles/PythonAutoTest/testPytest'])
