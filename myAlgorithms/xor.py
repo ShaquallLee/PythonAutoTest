70a5263475e21a8d3db3cc8483078af44dea4cbe
shaqulle lee <17631288710@163.com>
2021-11-14 17:03:16 +0800

    测试两种交换值的方法的速度

--- myAlgorithms/xor.py
+++ myAlgorithms/xor.py
@@ -4,8 +4,10 @@ Author: lishaogang
 version: 
 Date: 2021-11-14 16:18:20
 LastEditors: lishaogang
-LastEditTime: 2021-11-14 16:27:01
+LastEditTime: 2021-11-14 16:46:02
 '''
+
+import time
 
 #在一个数组中有a、b两个数只出现了一次，而其它的数都出现了2次，找出这两个数a和b
 def findAB(arr):
@@ -22,6 +24,20 @@ def findAB(arr):
     return a, xor^a
 
 
+
+def testABChangeTime():
+    '''测试a、b交换值两种方式所用的时间'''
+    start = time.time()
+    a = 10
+    b=100
+    for i in range(1000000):
+        a,b=b,a
+        # a = a^b
+        # b = a^b
+        # a = a^b
+    print(time.time()-start)
+
 if __name__ == '__main__':
-    arr = [8,1,2,3,5,1,2,5]
-    print(findAB(arr))
+    # arr = [8,1,2,3,5,1,2,5]
+    # print(findAB(arr))
+    testABChangeTime()
